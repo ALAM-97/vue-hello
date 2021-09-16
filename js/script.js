13d7d4f1bafcba33e1441b5c597405d564160beb
ALAM-97 <alexamara97@gmail.com>
2021-09-16 15:45:01 +0200

    Exercise Completed

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const root = new Vue({
+    el: '#root',
+    data: {
+        name: '',
+        surname: '',
+        email: '',
+        number: '',
+        image: 'https://cdn141.picsart.com/362742338079201.jpg?type=webp&to=min&r=640'
+    }
+})
